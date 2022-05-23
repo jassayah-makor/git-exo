c4d3b8f061dbceddd6d471361f337a114007f25d
kgoncharov-makor <85167075+kgoncharov-makor@users.noreply.github.com>
2022-05-23 14:37:08 +0300

    Add poopoo fn

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const saySomething = text => {
-  console.log(text)
+const saySomething = (text) => {
+	console.log(text)
 }
 
-const replyToAll = text => {
-
-}
+const replyToAll = (text) => {}
 
 const stopTalking = () => {
-  const num = '1' + '1'
-  console.log(num)
+	const num = '1' + '1'
+	console.log(num)
+}
+
+const poopoo = (size) => {
+	let dataToSend = ''
+
+	switch (size) {
+		case 'small':
+			dataToSend = '💩'
+			break
+		case 'medium':
+			dataToSend = '💩💩'
+			break
+		case 'large':
+			dataToSend = '💩💩💩'
+			break
+		default:
+			dataToSend = '💩'
+			break
+	}
+	console.log(dataToSend)
 }
