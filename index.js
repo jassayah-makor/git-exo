03efaa61c6893197696e09376a3a879af6edb254
aavisrur-makor <aavisrur@makor-capital.com>
2022-05-23 14:46:24 +0300

    gx-#1 Modifying assiginment

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const saySomething = text => {
-  console.log(text)
-}
+const saySomething = (text) => {
+  console.log(text);
+};
 
-const replyToAll = text => {
-
-}
 
 const stopTalking = () => {
-  const num = '1' + '1'
-  console.log(num)
-}
+  const num = "1" + "1";
+  console.log(num);
+};
+
+const savaMec = (name) => {
+  console.log("Sava mec " + name);
+};
