976cec5316d090cd3bb1a5787464a432e3c7d88e
dmizrahi <dmizrahi@makor-capital.com>
2022-05-23 14:40:03 +0300

    first one

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const saySomething = text => {
-  console.log(text)
-}
+const saySomething = (text) => {
+  console.log(text);
+};
 
-const replyToAll = text => {
-
-}
+const replyToAll = (text) => {};
 
 const stopTalking = () => {
-  const num = '1' + '1'
-  console.log(num)
-}
+  const num = "1" + "1";
+  console.log(num);
+  ("rami");
+};
