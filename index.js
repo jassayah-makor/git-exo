48c7c751b3ce66c785001d715bd92cf02e0ee3b7
dbYaakov-makor <drorby1996@gmail.com>
2022-05-23 14:41:15 +0300

    wirdo

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const saySomething = text => {
+const saySomething = (text) => {
   console.log(text)
 }
 
-const replyToAll = text => {
-
-}
+const replyToAll = (text) => {}
 
 const stopTalking = () => {
   const num = '1' + '1'
   console.log(num)
 }
+
+setTimeout(() => {
+  console.log('so wake me up when its all over ')
+  console.log(':)')
+}, 3000)
