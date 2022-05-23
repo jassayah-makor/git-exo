54d0da9174787af377bc3d4496ab96231554d880
ybar-makor <85983306+ybar-makor@users.noreply.github.com>
2022-05-23 14:42:36 +0300

    eX-#1 added yisrael function

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 const saySomething = (text) => {
-	console.log(text)
+  console.log(text)
 }
 
 const replyToAll = (text) => {}
 
 const stopTalking = () => {
-	const num = '1' + '1'
-	console.log(num)
+  const num = '1' + '1'
+  console.log(num)
 }
 
 const poopoo = (size) => {
-	let dataToSend = ''
+  let dataToSend = ''
 
-	switch (size) {
-		case 'small':
-			dataToSend = '💩'
-			break
-		case 'medium':
-			dataToSend = '💩💩'
-			break
-		case 'large':
-			dataToSend = '💩💩💩'
-			break
-		default:
-			dataToSend = '💩'
-			break
-	}
-	console.log(dataToSend)
+  switch (size) {
+    case 'small':
+      dataToSend = '💩'
+      break
+    case 'medium':
+      dataToSend = '💩💩'
+      break
+    case 'large':
+      dataToSend = '💩💩💩'
+      break
+    default:
+      dataToSend = '💩'
+      break
+  }
+  console.log(dataToSend)
+}
+
+const yisraelFunction = () => {
+	return true
 }
