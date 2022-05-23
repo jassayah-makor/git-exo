a0dc2e9e82189fa303995a690ed6624fbcbe5c40
ykabariti-Makor <ykabariti@makor-capital.com>
2022-05-23 14:50:13 +0300

    gx-#1 modify assignment - updated stopTalking func and added makeCoffe func

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-const saySomething = text => {
+const saySomething = (text) => {
   console.log(text)
 }
 
-const replyToAll = text => {
+const replyToAll = (text) => {}
 
+const stopTalking = (talking) => {
+  if (talking) {
+    console.log('Shut up!')
+  } else {
+    console.log('Ahla gever :)')
+  }
+  return
 }
 
-const stopTalking = () => {
-  const num = '1' + '1'
-  console.log(num)
+const makeCoffee = (sugerCount) => {
+  try {
+    if (sugerCount > 1) {
+      console.log('Are you sure its too much sugar!')
+    } else {
+      console.log('Greattt Coffe is on the way')
+    }
+    return
+  } catch (error) {
+    console.log('errorrrrrrrrr')
+  }
 }
