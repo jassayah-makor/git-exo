2a124ae5c2594ec62c3770af22e4ff47d725cad0
Eliyahu Hillel <ehilel@makor-capital.com>
2022-05-24 14:32:22 +0300

    FIX: ex-#1 add fibonacci func

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-(() =>{
-  throw new Error('Bye Bye')
-})()
+const saySomething = text => {
+  console.log(text)
+}
+
+const replyToAll = text => {
+
+}
+
+const stopTalking = () => {
+  const num = '1' + '1'
+  console.log(num)
+}
+
+function* fibonacci() {
+  let [prev, curr] = [0, 1];
+  for (;;) {
+      [prev, curr] = [curr, prev + curr];
+      yield curr;
+  }
+}
