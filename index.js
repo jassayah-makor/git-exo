e685c00babbb8c1eef0742a6d806d8318aa192c0
ybar-makor <85983306+ybar-makor@users.noreply.github.com>
2022-05-24 15:08:52 +0300

    Revert "Closes #1"

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
-const saySomething = (text) => {
+const saySomething = text => {
   console.log(text)
 }
 
-const stopTalking = () => {
-  const num = '1' + '1'
-  console.log(num)
-}
+const replyToAll = text => {
 
-const poopoo = (size) => {
-  let dataToSend = ''
-
-  switch (size) {
-    case 'small':
-      dataToSend = '💩'
-      break
-    case 'medium':
-      dataToSend = '💩💩'
-      break
-    case 'large':
-      dataToSend = '💩💩💩'
-      break
-    default:
-      dataToSend = '💩'
-      break
-  }
-  console.log(dataToSend)
-}
-
-const yisraelFunction = () => {
-	return true
 }
 
 const stopTalking = () => {
